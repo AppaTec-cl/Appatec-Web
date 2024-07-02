8e4d6b5abe202cf95aa72079e96c1de469758351
Cyphound <cyphound@gmail.com>
2024-07-02 16:05:21 -0400

    Appatec-Web: Re-Init project

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/Home';
-import Features from './components/Features';
-import Download from './components/Download';
-import Footer from './components/Footer';
-import './App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Header />
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/features" component={Features} />
-                    <Route path="/download" component={Download} />
-                    <Route path="/contact" component={Footer} />
-                </Switch>
-                <Footer />
-            </div>
-        </Router>
-    );
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
