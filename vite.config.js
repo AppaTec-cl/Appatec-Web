6192d8dac4bb2d570576fa295ab5d2a54b5e2da7
Benjamin Morales <benja.mc12@gmail.com>
2024-07-02 17:18:29 -0400

    1.3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Detectar el entorno (desarrollo o producción)
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   plugins: [react()],
-  base: '/Appatec-Web/', // Asegúrate de que esto sea correcto
+  base: isProd ? '/Appatec-Web/' : '/', // Usar '/' para desarrollo y '/Appatec-Web/' para producción
 });
