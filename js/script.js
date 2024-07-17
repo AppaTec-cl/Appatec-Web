3a8190d16de496bfcbe530657552a7f94612dc15
Benjamin Morales <benja.mc12@gmail.com>
2024-07-17 14:00:04 -0400

    3.0

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-function downloadInstaller() {
-    window.location.href = 'path/to/your/installer.exe';
-}
-
-function downloadUserManual() {
-    window.location.href = 'path/to/your/user-manual.pdf';
-}
-
-function downloadInstallationManual() {
-    window.location.href = 'path/to/your/installation-manual.pdf';
-}
-
 function updateThemeImages() {
     var theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'Oscuro' : 'Claro';
     document.getElementById('logo').src = theme === 'Oscuro' ? 'img/AppaTecLogoOscuro1-4.png' : 'img/AppaTecLogoOscuro1-4.png';
