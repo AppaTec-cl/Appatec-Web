cf11030f299376d64bbcb49640235e0aa2097c6a
Benjamin Morales <benja.mc12@gmail.com>
2024-07-16 22:01:34 -0400

    funciona esta vez (VEZ NRO MIL QUE HAGO LA PAG XDD)

--- js/script.js
+++ js/script.js
@@ -1,41 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let currentTheme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-    document.body.setAttribute('data-theme', currentTheme);
+function downloadInstaller() {
+    window.location.href = 'path/to/your/installer.exe';
+}
 
-    const logo = document.getElementById('logo');
-    updateLogo(currentTheme);
+function downloadUserManual() {
+    window.location.href = 'path/to/your/user-manual.pdf';
+}
 
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
-        currentTheme = event.matches ? 'dark' : 'light';
-        document.body.setAttribute('data-theme', currentTheme);
-        updateLogo(currentTheme);
-    });
+function downloadInstallationManual() {
+    window.location.href = 'path/to/your/installation-manual.pdf';
+}
 
-    function updateLogo(theme) {
-        if (theme === 'dark') {
-            logo.src = 'img/AppaTecLogoOscuro1-4.png';
-        } else {
-            logo.src = 'img/AppaTecLogoClaro1-2.png';
-        }
-    }
+function updateThemeImages() {
+    var theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'Oscuro' : 'Claro';
+    document.getElementById('logo').src = theme === 'Oscuro' ? 'img/AppaTecLogoOscuro1-4.png' : 'img/AppaTecLogoClaro1-2.png';
+    document.querySelector('.main-header').style.backgroundImage = theme === 'Oscuro' ? 'url(img/AppaTecFondoOscuro1-3.png)' : 'url(img/AppaTecFondoClaro1-1.png)';
+}
 
-    function applyFade() {
-        const heroSectionHeight = document.querySelector('.hero-section').offsetHeight;
-        const scrollPosition = window.scrollY;
-
-        const fadePoint = scrollPosition / heroSectionHeight;
-        const opacity = Math.min(1, Math.max(0, fadePoint));
-
-        if (currentTheme === 'dark') {
-            document.body.style.backgroundImage = `linear-gradient(to bottom, rgba(0, 0, 0, ${opacity}) 0%, rgba(0, 0, 0, 1) 100%), var(--background-image-dark)`;
-        } else {
-            document.body.style.backgroundImage = `linear-gradient(to bottom, rgba(255, 255, 255, ${opacity}) 0%, rgba(255, 255, 255, 1) 100%), var(--background-image-light)`;
-        }
-    }
-
-    // Aplicar el fade al cargar la página
-    applyFade();
-
-    // Aplicar el fade al hacer scroll
-    window.addEventListener('scroll', applyFade);
-});
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateThemeImages);
+document.addEventListener('DOMContentLoaded', updateThemeImages);
